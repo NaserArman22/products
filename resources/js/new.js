89f306660f404026f6b356be5ef4fd099f55c7bf
NaserArman22 <mdnaserarman57@gmail.com>
2025-06-02 01:10:10 +0600

    nr

---
+++ resources/js/new.js
@@ -0,0 +1,19 @@
+ <script type="module">
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyDYXLfvfPLhzblNLiLCvVqJlFxXP7JM0Lo",
+      authDomain: "fifth-sol-413518.firebaseapp.com",
+      projectId: "fifth-sol-413518",
+      storageBucket: "fifth-sol-413518.firebasestorage.app",
+      messagingSenderId: "55667876032",
+      appId: "1:55667876032:web:e2701fb46e0f1e1a7acd25"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+  </script>
